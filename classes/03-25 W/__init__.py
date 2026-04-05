ec10f121605792aa5d4c40ff07ed523ef7cbabe4
888lizabeth <tranelizabeth0201@gmail.com>
2026-04-05 19:51:31 -0400

    hw

--- classes/03-25 W/__init__.py
+++ classes/03-25 W/__init__.py
@@ -11,5 +11,22 @@ port = {'cash': 10000.00,
                         'price': 420.00)}}
 
 def buy_stock(port):
+    sys = input ('Enter stock symbol you want to buy: ')
+    
     txt = 'Enter shares you want to buy'
-    ss
+    while True:
+        try:
+            shares = int(input(txt))
+            break
+        except ValueError:
+            txt = 'You must enter an integer'
+
+    txt = f'Enter price of (sym):'
+    while True:
+        try:
+            price = float(input(txt))
+            break
+        except ValueError:
+            txt = 'You must enter a float'
+    
+    if shares = price > port ['cash']:
